c8239e34125d3bc8e4de33c073e29a45695e63b2
Aninda Paul <109781193+Aninda12@users.noreply.github.com>
2025-08-19 22:29:59 +0530

    Update mineBlock.js

--- backend/scripts/mineBlock.js
+++ backend/scripts/mineBlock.js
@@ -1,11 +1,14 @@
 async function main() {
     // Mine a block
     await network.provider.send("evm_mine");
+    
     console.log("Mined one block!");
+    
   }
   
   main()
     .then(() => process.exit(0))
+      
     .catch((error) => {
       console.error(error);
       process.exit(1);
